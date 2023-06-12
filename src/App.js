c4c3846cb95fbb49a87d4be5253ac69d308db425
kligan <kbraganza@freeWheel.com>
2023-06-12 19:05:21 +0100

    added keyboard with layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Lightning, Utils } from '@lightningjs/sdk'
-import { InputField } from '@lightningjs/ui'
+import { Lightning, Utils, Colors } from '@lightningjs/sdk'
+import { InputField, Keyboard, Key as BaseKey } from '@lightningjs/ui'
 
 export default class App extends Lightning.Component {
   static getFonts() {
@@ -8,18 +8,12 @@ export default class App extends Lightning.Component {
 
   static _template() {
     return {
+      ...super._template(),
       Background: {
         w: 1920,
         h: 1080,
         rect: true,
-      },
-      InputFieldWrapper: {
-        x: 600,
-        y: 150,
-        rect: true,
-        h: 50,
-        w: 460,
-        InputField: { type: InputField },
+        color: 0xffff0000,
       },
       Text: {
         mount: 1,
@@ -30,6 +24,23 @@ export default class App extends Lightning.Component {
           fontFace: 'Regular',
           fontSize: 64,
           textColor: 0xbbffffff,
+        },
+      },
+      Content: {
+        InputFieldWrapper: {
+          x: 600,
+          y: 150,
+          rect: true,
+          h: 50,
+          w: 460,
+          InputField: { x: 20, y: 20, type: InputField },
+        },
+        Keyboard: {
+          y: 500,
+          w: 500,
+          type: Keyboard,
+          config: virtualKeyboardConfig,
+          currentLayout: 'abc',
         },
       },
     }
@@ -50,4 +61,54 @@ export default class App extends Lightning.Component {
       })
       .start()
   }
+
+  _setup() {
+    const inputField = this.tag('InputField')
+    this.tag('Keyboard').inputField(inputField)
+  }
+
+  _getFocused() {
+    return this.tag('Keyboard')
+  }
+
+  static get icon() {
+    return 'images/keyboard.png'
+  }
+}
+
+const virtualKeyboardConfig = {
+  layouts: {
+    123: [
+      ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+      [
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z',
+      ],
+      ['Space', 'Backspace'],
+    ],
+  },
 }
